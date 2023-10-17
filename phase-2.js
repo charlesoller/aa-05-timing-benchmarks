94dfed536e4be9dda08084562c0b8923c6d3403f
Valentina <vfer0890@gmail.com>
2023-10-17 18:47:03 -0400

    phase two completed

--- phase-2.js
+++ phase-2.js
@@ -2,13 +2,24 @@ const [addNums, addManyNums] = require("./phase-1");
 
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
-  // Fill this in
-
+  let newArr = [];
+  let x = increment;
+  for (let i = 0; i < 10; i++) {
+    newArr.push(addNums(x));
+    x += increment;
+  }
+return newArr;
 }
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
-  // Fill this in
+  let newArr = [];
+  let x = increment;
+  for (let i = 0; i < 10; i++) {
+    newArr.push(addManyNums(x));
+    x += increment;
+  }
+return newArr;
 
 }
 
